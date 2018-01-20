6523d45a2e1d1a2390ed203d509601d08cbbb3a0
Ankit Tanna <ankittanna@hotmail.com>
2018-01-20 19:46:27 +0530

    issue with route middleware

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var configureMiddleWare = require('../middleware/api.middleware');
+/*var configureMiddleWare = require('../middleware/api.middleware');*/
 
 var user = require('./apis/user.routes');
 var login = require('./apis/login.routes');
